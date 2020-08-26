20652dbc23e6c35e8f149457f4374fe36bb00f21
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-08-27 11:06:13 +1200

    Fields and Constructor

---
+++ 102-PracticeTest1-Temperatures/Reading.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _102_PracticeTest1_Temperatures
+{
+    class Reading
+    {
+        // fields
+        private string _date;
+        private double _high;
+        private double _low;
+
+        // Constructor
+        /// <summary>
+        /// Initialises the object to the values passed in
+        /// </summary>
+        /// <param name="date">The date of the reading</param>
+        /// <param name="high">The high value for that date</param>
+        /// <param name="low">The low value for that date</param>
+        public Reading(string date, double high, double low)
+        {
+            if (date != "")
+            {
+                _date = date;
+            }
+            else
+            {
+                throw new Exception("The date must be specified");
+            }
+
+
+        }
+    }
+}
