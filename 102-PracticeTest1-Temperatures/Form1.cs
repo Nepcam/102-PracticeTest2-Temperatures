19380708301ba4820f6d9f5c6c02b319f2617d2a
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-08-27 10:55:59 +1200

    Add project files.

---
+++ 102-PracticeTest1-Temperatures/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _102_PracticeTest1_Temperatures
+{
+    public partial class Form1 : Form
+    {
+        //Name:
+        //ID:
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
