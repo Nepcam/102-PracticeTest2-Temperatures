13346a2b631bdd92fcf1ff1be93b0e84bd595f94
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-08-27 12:28:28 +1200

    working on the Reading class

--- 102-PracticeTest1-Temperatures/Reading.cs
+++ 102-PracticeTest1-Temperatures/Reading.cs
@@ -8,12 +8,12 @@ namespace _102_PracticeTest1_Temperatures
 {
     class Reading
     {
-        // fields
+        // fields*******************************************************
         private string _date;
         private double _high;
         private double _low;
 
-        // Constructor
+        // Constructor**************************************************
         /// <summary>
         /// Initialises the object to the values passed in
         /// </summary>
@@ -22,6 +22,7 @@ namespace _102_PracticeTest1_Temperatures
         /// <param name="low">The low value for that date</param>
         public Reading(string date, double high, double low)
         {
+            // error check 
             if (date != "")
             {
                 _date = date;
@@ -30,8 +31,45 @@ namespace _102_PracticeTest1_Temperatures
             {
                 throw new Exception("The date must be specified");
             }
+            _high = high;
+            _low = low;
+        }
 
+        // Properties***************************************************
 
+        /// <summary>
+        /// Gets the date of the reading
+        /// </summary>
+        public string Date
+        {
+            get { return _date; }
+        }
+
+        /// <summary>
+        /// Gets and sets the high temperature value for that date
+        /// </summary>
+        public double High
+        {
+            get { return _high; }
+            set { _high = value; }
+        }
+
+        /// <summary>
+        /// Gets and sets the low value for that date
+        /// </summary>
+        public double Low
+        {
+            get { return _low; }
+            set { _low = value; }
+        }
+
+        /// <summary>
+        /// Gets all information about a reading
+        /// </summary>
+        /// <returns>All information as a neatly padded out string</returns>
+        public override string ToString()
+        {
+            return Date.PadRight(10) + High.ToString().PadRight(6) + Low;
         }
     }
 }
